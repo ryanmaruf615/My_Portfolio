310017d19d1f39028288c84b3e36f3f72f4b24f2
Md Maruf Hossain <37552845+ryanmaruf615@users.noreply.github.com>
2025-08-08 01:34:59 +0200

    project added

--- src/Components/MyProject.jsx
+++ src/Components/MyProject.jsx
@@ -22,7 +22,21 @@ import vs from '../assets/skill/vs.png'
 import git from '../assets/skill/git.png'
 import jwt from '../assets/skill/jwt.png'
 import fire from '../assets/skill/fire.png'
-import Kubernetes from '../assets/skill/Kubernetes.png'
+
+import Kubernetes from '../assets/skill/Kubernetes.png';
+
+import docker from '../assets/skill/docker.png'
+
+
+import Ansible from '../assets/skill/Ansible.png'
+import argocd from '../assets/skill/argocd.png'
+import aws from '../assets/skill/aws.png'
+import gitops from '../assets/skill/gitops.png'
+import grafana from '../assets/skill/grafana.png'
+import jenkins from '../assets/skill/jenkins.png'
+import prometheus from '../assets/skill/prometheus.png'
+import terraform from '../assets/skill/terraform.png'
+
 
 
 
@@ -73,6 +87,35 @@ const MyProject = ({ p1, p2, p3, p4, p5 }) => {
                     </div>
                 </div>
 
+<div className="hero  bg-base-200 rounded-lg hover:-translate-y-1 duration-500" data-aos="fade-up">
+                    <div className="hero-content flex-col xl:flex-row">
+                        <div className='p-3'>
+                            <div className={`w-[300px] rounded-lg h-[250px] ease-in-out duration-[5000ms] bg-cover bg-top hover:bg-bottom`} style={{ backgroundImage: `url(${p3})` }}>
+
+                            </div>
+                        </div>
+                        <div>
+                            <h1 className="text-4xl font-bold text-gray-300">WunderLust</h1>
+                            <p className="py-6">Automated end-to-end deployment on AWS EKS with ArgoCD, Terraform, and Jenkins. Integrated CI/CD pipelines, implemented GitOps for seamless sync, and set up monitoring with Prometheus and Grafana — boosting scalability, reliability, and operational efficiency.</p>
+                            <div className='flex items-center gap-3'>
+                                <p className='flex items-center gap-1'>  <span className='text-2xl'><FaGithubSquare /></span><a className='link-hover' href="https://github.com/ryanmaruf615/Wanderlust-Mega-Project" target='_blank'>Client </a></p>                               
+                            </div>
+
+                            <div className='flex items-center flex-wrap gap-3 mt-5'>
+                                <img className='w-[30px]'  src={aws} alt="" />  
+                                <img className='w-[30px]'  src={Kubernetes} alt="" />                           
+                                <img className='w-[30px]'  src={docker} alt="" />
+                                <img className='w-[30px]'  src={jenkins} alt="" />
+                                <img className='w-[30px]'  src={argocd} alt="" />
+                                <img className='w-[30px]'  src={prometheus} alt="" />
+                                <img className='w-[30px]'  src={Ansible} alt="" />
+                                <img className='w-[30px] bg-white p-1 rounded-full'  src={gitops} alt="" />
+                                <img className='w-[30px]'  src={grafana} alt="" />
+                                <img className='w-[30px]'  src={terraform} alt="" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
                 
                 
 
